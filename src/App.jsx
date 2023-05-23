8cfbda7cb80721a72f47b5e2d7f24c89558819ef
Karthik R <karthikclgid@gmail.com>
2023-05-23 19:37:38 +0530

    Edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,52 +14,101 @@ import {
   Projects,
 } from './components';
 
+
 const App = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const checkIfMobile = () => {
-      const isMobile = window.innerWidth <= 1000; // Set your desired mobile width threshold 
-      setIsMobile(isMobile);
-    };
-
-    checkIfMobile();
-    window.addEventListener('resize', checkIfMobile);
-
-    return () => {
-      window.removeEventListener('resize', checkIfMobile);
-    };
-  }, []);
-
   return (
     <BrowserRouter>
-      {isMobile ? (
-        <div class="flex justify-center items-center h-screen bg-white ">
-          <div class="text-center text-black font-bold">
-            <h1>The Graphics of the site is not supported by mobile phones.</h1>
-            <h1>Use a desktop or a laptop to access it.</h1>
-          </div>
+
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
         </div>
-      ) : (
-        <div className='relative z-0 bg-primary'>
-          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-            <Navbar />
-            <Hero />
-          </div>
-          <About />
-          <Education />
-          <Skills />
-          <Projects />
-          <Connect />
-          <div className='relative z-0'>
-            <Contact />
-            <StarsCanvas />
-          </div>
-          <Copyrights />
+        <About />
+        <Education />
+        <Skills />
+        <Projects />
+        <Connect />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
         </div>
-      )}
+        <Copyrights />
+      </div>
+
     </BrowserRouter>
   );
 };
 
+
 export default App;
+
+
+
+// If You want your site be accessible only Through Desktop, Use the Below code not the above one
+
+// import React, { useEffect, useState } from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+
+// import {
+//   About,
+//   Contact,
+//   Connect,
+//   Hero,
+//   Navbar,
+//   Copyrights,
+//   StarsCanvas,
+//   Education,
+//   Skills,
+//   Projects,
+// } from './components';
+
+// const App = () => {
+//   const [isMobile, setIsMobile] = useState(false);
+
+//   useEffect(() => {
+//     const checkIfMobile = () => {
+//       const isMobile = window.innerWidth <= 1000; // Set your desired mobile width threshold 
+//       setIsMobile(isMobile);
+//     };
+
+//     checkIfMobile();
+//     window.addEventListener('resize', checkIfMobile);
+
+//     return () => {
+//       window.removeEventListener('resize', checkIfMobile);
+//     };
+//   }, []);
+
+//   return (
+//     <BrowserRouter>
+//       {isMobile ? (
+//         <div class="flex justify-center items-center h-screen bg-white ">
+//           <div class="text-center text-black font-bold">
+//             <h1>The Graphics of the site is not supported by mobile phones.</h1>
+//             <h1>Use a desktop or a laptop to access it.</h1>
+//           </div>
+//         </div>
+//       ) : (
+//         <div className='relative z-0 bg-primary'>
+//           <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+//             <Navbar />
+//             <Hero />
+//           </div>
+//           <About />
+//           <Education />
+//           <Skills />
+//           <Projects />
+//           <Connect />
+//           <div className='relative z-0'>
+//             <Contact />
+//             <StarsCanvas />
+//           </div>
+//           <Copyrights />
+//         </div>
+//       )}
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
